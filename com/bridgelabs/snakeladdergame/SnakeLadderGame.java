c6c25fe47429c0155e7092670641bfde67a3305d
Kunalpatil009 <009kunalpatil@gmail.com>
2022-08-06 21:25:49 +0530

    game start position added

---
+++ com/bridgelabs/snakeladdergame/SnakeLadderGame.java
@@ -0,0 +1,9 @@
+package com.bridgelabs.snakeladdergame;
+import java.util.Random;
+
+public class SnakeLadderGame {
+    public static void main(String[] args) {
+                System.out.println("Start The Game By Rolling The Dice From Position Zero (0)");
+
+    }
+}
